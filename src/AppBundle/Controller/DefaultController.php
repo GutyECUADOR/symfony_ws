320b38a19024291ac1d0755037ddb52dec06e5d5
Jose Gutierrez <gutiecuador@gmail.com>
2018-09-20 09:49:37 -0500

    Pruebas de conexion a SQL Server
    Estas realizadas las pruebas con el conector PDO_SQLSRV v3.2 y se recuperan los datos correctamente

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Equipo;    
 
 class DefaultController extends Controller
 {
@@ -14,8 +15,25 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/dashboard/home.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+     /**
+     * @Route("/mantenimientos/{equipo}", name="mantenimientos")
+     */
+    public function mantenimientosAction($equipo='todos')
+    {
+        $repositoryEquipos = $this->getDoctrine()->getRepository(Equipo::class);
+        $equipos = $repositoryEquipos->findAll();
+       
+        return $this->render('default/dashboard/mantenimientos.html.twig', array(
+            'equipos' => $equipos));
+    }
+
+    public function createEquipo(){
+        
+    }
+
 }
---
+++ src/AppBundle/Entity/Equipo.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Equipo
+ *
+ * @ORM\Table(name="equipo")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EquipoRepository")
+ */
+class Equipo
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="codProducto", type="string", length=25, unique=true)
+     */
+    private $codProducto;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="precio", type="decimal", precision=5, scale=2)
+     */
+    private $precio;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="text")
+     */
+    private $descripcion;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fechaCreacion", type="datetime")
+     */
+    private $fechaCreacion;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imagen", type="string", length=255)
+     */
+    private $imagen;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isActivo", type="boolean")
+     */
+    private $isActivo;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set codProducto
+     *
+     * @param string $codProducto
+     *
+     * @return Equipo
+     */
+    public function setCodProducto($codProducto)
+    {
+        $this->codProducto = $codProducto;
+
+        return $this;
+    }
+
+    /**
+     * Get codProducto
+     *
+     * @return string
+     */
+    public function getCodProducto()
+    {
+        return $this->codProducto;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Equipo
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set precio
+     *
+     * @param string $precio
+     *
+     * @return Equipo
+     */
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    /**
+     * Get precio
+     *
+     * @return string
+     */
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     *
+     * @return Equipo
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set fechaCreacion
+     *
+     * @param \DateTime $fechaCreacion
+     *
+     * @return Equipo
+     */
+    public function setFechaCreacion($fechaCreacion)
+    {
+        $this->fechaCreacion = $fechaCreacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaCreacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaCreacion()
+    {
+        return $this->fechaCreacion;
+    }
+
+    /**
+     * Set imagen
+     *
+     * @param string $imagen
+     *
+     * @return Equipo
+     */
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    /**
+     * Get imagen
+     *
+     * @return string
+     */
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    /**
+     * Set isActivo
+     *
+     * @param boolean $isActivo
+     *
+     * @return Equipo
+     */
+    public function setIsActivo($isActivo)
+    {
+        $this->isActivo = $isActivo;
+
+        return $this;
+    }
+
+    /**
+     * Get isActivo
+     *
+     * @return bool
+     */
+    public function getIsActivo()
+    {
+        return $this->isActivo;
+    }
+}
